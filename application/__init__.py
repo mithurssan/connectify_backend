b99bce6b72d24942ba3956db9f711b69a6e241fb
Tom Burzynski <tellur.beryl@gmail.com>
2023-06-12 15:52:44 +0100

    add CH proxy to register business

--- application/__init__.py
+++ application/__init__.py
@@ -1,3 +1,4 @@
+# from decouple import config
 from flask import Flask, jsonify
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
@@ -7,14 +8,18 @@ app = Flask(__name__)
 CORS(app)
 
 app.config["SECRET_KEY"] = environ.get("KEY")
-app.config['SQLALCHEMY_DATABASE_URI'] = environ.get("DB_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = environ.get("DB_URL")
+# app.config.from_object(config("APP_SETTINGS"))
 db = SQLAlchemy(app)
 
 from application.routes import UsersRoutes
+from application.routes import CompaniesHouseProxy
+
 
 @app.route("/")
 def index():
     return jsonify({"message": "Welcome to the Connectify backend!"})
 
-app.register_blueprint(UsersRoutes.user, url_prefix= "/users")
 
+app.register_blueprint(UsersRoutes.user, url_prefix="/users")
+app.register_blueprint(CompaniesHouseProxy.proxy, url_prefix="/api")
