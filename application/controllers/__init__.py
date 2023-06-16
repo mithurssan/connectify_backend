4abc8f7fcc59df5d2ab52ede576ca940caf7d824
mithurssan <126930607+mithurssan@users.noreply.github.com>
2023-06-16 11:39:55 +0100

    complete CRUD for posts

--- application/controllers/__init__.py
+++ application/controllers/__init__.py
@@ -3,5 +3,6 @@ from .HolidayController import HolidayController
 from .BusinessController import BusinessController
 from .JournalController import JournalController
 from .RotaController import RotaController
+from .PostController import PostController
 
-__all__ = ["UserController", "HolidayController", "BusinessController", "JournalController", "RotaController"]
+__all__ = ["UserController", "HolidayController", "BusinessController", "JournalController", "RotaController", "PostController"]
---
+++ application/models/Post.py
@@ -0,0 +1,35 @@
+from application import db
+
+class Post(db.Model):
+    __tablename__ = "posts"
+    post_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.String(32), db.ForeignKey("users.user_id"), nullable=False)
+    business_id = db.Column(db.String(32), db.ForeignKey("businesses.business_id"), nullable=False)
+    post_title = db.Column(db.String(50), nullable=False)
+    post_content = db.Column(db.String(350), nullable=False)
+
+    def __init__(self, user_id, business_id, post_title, post_content):
+        self.user_id = user_id
+        self.business_id = business_id
+        self.post_title = post_title
+        self.post_content = post_content
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @staticmethod
+    def get_all():
+        return Post.query.all()
+
+    @staticmethod
+    def get_by_id(post_id):
+        return db.session.get(Post, post_id)
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
